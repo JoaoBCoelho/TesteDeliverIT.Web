4127c9e376f54eabeba2fcff68e630572f74e940
João Coelho <joaoborgesc@hotmail.com>
2020-12-10 15:39:05 -0300

    Finalização do projeto web com criação da service/environment

--- src/app/conta/conta-form/conta-form.component.ts
+++ src/app/conta/conta-form/conta-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { Conta } from 'src/app/models/conta';
+import { ContaService } from 'src/app/services/conta.service';
 
 @Component({
   selector: 'app-conta-form',
@@ -10,22 +12,29 @@ import { Validators } from '@angular/forms';
 
 export class ContaFormComponent implements OnInit {
   profileForm = new FormGroup({
-    name: new FormControl('', Validators.required),
+    nome: new FormControl('', Validators.required),
     valorOriginal: new FormControl('', Validators.required),
     dataVencimento: new FormControl('', Validators.required),
     dataPagamento: new FormControl('', Validators.required),
   });
 
-  constructor() { }
+  constructor(
+    public contaService: ContaService) { }
 
   ngOnInit(): void {
-    
+
   }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
-    this.profileForm.reset();
+    this.contaService.post(new Conta(this.profileForm.value)).subscribe(
+      data => {
+        this.profileForm.reset();
+      },
+      error => {
+            console.error(error);
+        });
   }
 
 }
